9245b5bb28fc284eccb5f51a53af603c082a18a9
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-05-20 08:43:12 +0530

    note: about file

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -3,3 +3,4 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+// this file makes sure that nextjs types are picked by typescript compiler
