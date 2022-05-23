22373967cbdfb8732b336a70a10b4ce09cbf5384
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-05-23 09:14:55 +0530

    component: navbar

---
+++ client/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./navbar.module.scss";
+import Link from "next/link";
+function Navbar() {
+  return (
+    <nav className={`${styles.navbar} justalign-bw`}>
+      <section className={styles.navbar__left}>
+        <Link href="/">
+          <h3 className="pointer">Track</h3>
+        </Link>
+      </section>
+      <section className={`${styles.navbar__right}`}>
+        <ul className="justalign-bw">
+          <Link href="/join/login">
+            <li className="pointer">Login</li>
+          </Link>
+          <Link href="/join/signup">
+            <li className="pointer">SignUp</li>
+          </Link>
+        </ul>
+      </section>
+    </nav>
+  );
+}
+
+export default Navbar;
