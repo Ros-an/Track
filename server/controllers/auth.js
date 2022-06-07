e096d06585e6b5e4656df7afcf876185d10102f5
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-07 09:40:44 +0530

    route: register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,34 @@
-export const register = (req, res) => {
-    console.log(req.body);
-    res.json("this is register endpoin controller");
+import User from "../model/user";
+import {comparePassword,hashPassword} from "../utils/auth";
+
+
+export const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        // validation
+        if(!name) return res.status(400).send("Name is required.");
+        if(!password || password.length < 6) return res.status(400).send("Password should be of min 6 char long.");
+        let userExist = await User.findOne({email}).exec();
+        if(userExist) return res.status(400).send("Email already exist.");
+
+        // hash password
+        const hashedPassword = await hashPassword(password);
+
+        // create a user
+        const user = new User({
+            name, email, password: hashedPassword
+        }); 
+        //saving user at database
+        await user.save();
+        console.log("this is user", user);
+
+        return res.json({
+            status: 200,
+            message: "User registered successfully!"
+        })
+    } catch (err) {
+        console.log("Error", err);
+        return res.status(400).send("Error.Try Again!")
+    }
 }
