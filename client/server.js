2a06b3be769ed7292f46377bb82d18f846b0aa35
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-09 08:46:10 +0530

    note added

--- client/server.js
+++ client/server.js
@@ -1,3 +1,17 @@
+/**
+ * Nextjs setup custom server
+ * to use cookie based auth system we need to have both client and server running on same origin/domain
+ * we need to use proxy for that because our client/nextjs is running on 3000
+ * and our server is running on 8000
+ */
+
+/**
+ * use proxy in nextjs
+ * to use proxy we need to create custom server
+ * this is only for development mode
+ * in production, we will use same origin/domain so we don't have to worry about it
+ * we can simply run build then start next app
+ */
 const express = require("express");
 const {createProxyMiddleware} = require("http-proxy-middleware");
 const next = require("next");
