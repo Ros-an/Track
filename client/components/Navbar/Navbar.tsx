d8faea00197c4435f8a0a26e5d8efb28d6602322
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-12 11:10:26 +0530

    feat: logout, conditional auth link

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,22 +1,60 @@
 import React from "react";
 import styles from "./navbar.module.scss";
 import Link from "next/link";
+import { useCustomGlobalContext } from "../../context/GlobalContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { Avatar, Menu } from "antd";
+import { firstLetterOfString, splitStr } from "../../utils/global";
+import { SettingOutlined, AppstoreOutlined } from "@ant-design/icons";
+import Dropdown from "./Dropdown";
+
 function Navbar() {
+  const { state, dispatch } = useCustomGlobalContext();
+  const { user } = state;
+  const router = useRouter();
+  const handleLogout = async () => {
+    dispatch({ type: "LOGOUT" });
+    localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast.success(data.message);
+    router.push("/join/login");
+  };
   return (
     <nav className={`${styles.navbar} justalign-bw`}>
-      <section className={styles.navbar__left}>
+      <section className={`${styles.navbar__left} justalign-center`}>
         <Link href="/">
           <h3 className="pointer">Track</h3>
         </Link>
       </section>
       <section className={`${styles.navbar__right}`}>
         <ul className="justalign-bw">
-          <Link href="/join/login">
-            <li className="pointer">Login</li>
-          </Link>
-          <Link href="/join/signup">
-            <li className="pointer">Sign up</li>
-          </Link>
+          {user === null && (
+            <>
+              <Link href="/join/login">
+                <li className={`${styles.login} pointer`}>Login</li>
+              </Link>
+              <Link href="/join/signup">
+                <li className={`${styles.signup} pointer`}>Sign up</li>
+              </Link>
+            </>
+          )}
+          {user && (
+            <Dropdown
+              avatar={
+                <Avatar size={40} style={{ background: "#1c1d1f" }}>
+                  {`${firstLetterOfString(
+                    splitStr(user.name, " ")[0]
+                  )}${firstLetterOfString(splitStr(user.name, " ")[1])}`}
+                </Avatar>
+              }
+            >
+              <li className="pointer" onClick={handleLogout}>
+                Logout
+              </li>
+            </Dropdown>
+          )}
         </ul>
       </section>
     </nav>
