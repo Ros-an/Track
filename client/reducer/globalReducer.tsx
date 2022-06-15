f02f609b96e123ae38c8c856943ebbe9446421b2
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-15 21:11:03 +0530

    refactor: action interface

--- client/reducer/globalReducer.tsx
+++ client/reducer/globalReducer.tsx
@@ -1,24 +1,26 @@
 import { StateInterface } from "../context/GlobalContext";
 export interface ActionInterface {
-    payload: any;
-    type: string;
+  payload?: any;
+  type: string;
 }
 
-
-export function globalReducer(state: StateInterface, action: ActionInterface): StateInterface {
-    const {payload, type} = action;
-    switch(type){
-        case "LOGIN":
-            return {
-                ...state,
-                user: payload
-            }
-        case "LOGOUT":
-            return {
-                ...state,
-                user: null
-            }
-        default: 
-            return state;
-    }
+export function globalReducer(
+  state: StateInterface,
+  action: ActionInterface
+): StateInterface {
+  const { payload, type } = action;
+  switch (type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
 }
