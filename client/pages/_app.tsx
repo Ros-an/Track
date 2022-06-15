257c78eb536e34b1f321b4ac871beba6b6076272
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-15 21:12:12 +0530

    removed: global route

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar/Navbar";
 import "antd/dist/antd.css";
 import "../public/css/global.scss";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalContextProvider from "../context/GlobalContext";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -11,17 +11,15 @@ import { useEffect } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   console.log("myapp", router);
-  useEffect(()=> {
-    console.log("run");
-    
-if(router.pathname === "/join/signup" && !localStorage.getItem("user")){
-  router.replace(`/join/login?next=${router.pathname}`)
-  }
-  },[router.pathname])
-  
+  // useEffect(() => {
+  //   if (router.pathname === "/join/signup" && !localStorage.getItem("user")) {
+  //     router.replace(`/join/login?next=${router.pathname}`);
+  //   }
+  // }, [router.pathname]);
+
   return (
     <GlobalContextProvider>
-      <ToastContainer position="top-center" autoClose={1500}/>
+      <ToastContainer position="top-center" autoClose={1500} />
       <Navbar />
       <Component {...pageProps} />
     </GlobalContextProvider>
